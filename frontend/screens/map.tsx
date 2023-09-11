afdab4e4e0214336106eff38e098b3a20c9a1a7d
Julian Hay <julian.hay@gmx.de>
2023-09-11 17:49:39 +0200

    added popups and camera screens

--- frontend/screens/map.tsx
+++ frontend/screens/map.tsx
@@ -1,40 +1,114 @@
-import React, {UseState} from 'react';
-import { View, StyleSheet } from 'react-native';
-// import MapView, { Marker, Geojson } from 'react-native-maps';
-// import Countries from '../assets/countries'
+import React, { useState, useEffect } from 'react';
+import { View, StyleSheet, Image, TouchableOpacity, Dimensions, Linking} from 'react-native';
+import MapView, { Marker, Geojson} from 'react-native-maps';
+import Countries from '../assets/countries'
+import client from '../actions/client';
+import { CustomText } from '../components/text';
+import Modal from "react-native-modal";
+import { FontAwesome5 } from '@expo/vector-icons';
+import moment from 'moment';
 
 const MapScreen = () => {
-//     const countryList = ['France', 'Canada', 'China']
-//     const coordinates = [[120,40],[50,32],[12,143],[14,65]]
+  const [selectedEvent, setSelectedEvent] = useState(null);
+  const [isEventModalVisible, setEventModalVisible] = useState(false);
+  const [selectedImage, setSelectedImage] = useState(null);
+  const [isImageModalVisible, setImageModalVisible] = useState(false);
+  const [eventData,setEventData] = useState([])
+  const [imageData,setImageData] = useState([])
+  const [loading, setLoading] = useState(true)
+  useEffect(()=>{
+      client.get('/events').then((res) => setEventData(res.data))
+      .then(() => client.get('/pictures').then((res) => setImageData(res.data)))
+      .finally(() => setLoading(false))
+  },[]) 
 
-//     const countries = Countries.features.filter(feature =>
-//         countryList.includes(feature.properties.name)
-//       );
+  const countryList = ['Germany','Germany','Finland','Greece','Italy','Mexico','Brazil']
+  const countries = Countries.features.filter(feature =>
+    countryList.includes(feature.properties.name));
 
-//   const markers = coordinates.map((coordinate, index) => ({
-//     coordinate: {
-//       latitude: coordinate[0],
-//       longitude: coordinate[1],
-//     },
-//     title: countries[index],
-//   }));
-
+  const coloredCountries = countries.map((country,index)=> (
+    <Geojson key={index} 
+    geojson={{
+        type: 'FeatureCollection',
+        features: [country]}} 
+    strokeColor="#0568AE"
+    fillColor="#009FDB"
+    strokeWidth={2}
+    tappable={true}
+    onPress={() => {
+      setSelectedEvent(eventData[index]);
+      setEventModalVisible(true);
+    }}
+    /> 
+    ))
+  
+  const imageMarkers =  imageData.map((image, index) => (
+      <Marker key={index} 
+      coordinate={{longitude:parseFloat(image.long),latitude:parseFloat(image.lat)}}
+      image={require('../assets/maps_marker.png')}
+      onPress={()=>{
+        setSelectedImage(image)
+        setImageModalVisible(true)
+      }}>
+      </Marker>
+    ))
+  
   return (
+    loading ? <CustomText>Loading...</CustomText> :
     <View style={styles.container}>
-      {/* <MapView style={styles.map}>
-        {countries.map((country,index)=> (
-            <Geojson key={index} 
-            geojson={{
-                type: 'FeatureCollection',
-                features: [country]}} 
-            strokeColor="#0568AE"
-            fillColor="#009FDB"
-            strokeWidth={2} onPress={()=>('')}/>))}
-        {markers.map((marker, index) => (
-          <Marker key={index} coordinate={marker.coordinate} title={marker.title} />
-        ))}
-      </MapView> */}
+      <MapView style={styles.map}>
+        {coloredCountries}
+        {imageMarkers}
+      </MapView>
+
+      <Modal isVisible={isEventModalVisible} 
+      backdropOpacity={1}
+      onBackdropPress={() => setEventModalVisible(false)}
+      >
+        <View style={{alignItems:'center',height:'100%', justifyContent:'center'}}>
+          {selectedEvent && (
+            <>
+              <TouchableOpacity style={{position:'absolute', top:30, right:10,width:30,height:30, backgroundColor:'white',borderRadius:50, alignItems:'center',justifyContent:'center'}} onPress={() => setEventModalVisible(false)}>
+                <CustomText fontWeight='bold' fontSize={24}>X</CustomText>
+              </TouchableOpacity>
+              <CustomText color='white' fontSize={24} fontWeight='bold'>{selectedEvent.title}</CustomText>
+              <View style={{margin:-40}}>
+                <Image source={{uri:selectedEvent.image}} style={{width:Dimensions.get('window').width*0.4, height:Dimensions.get('window').height*0.4}} resizeMode='contain'/>
+              </View>
+              <CustomText color='white' fontSize={24} fontWeight='bold'>{moment(selectedEvent.start,'YYYY-MM-DD').format('DD.MM.YYYY')}</CustomText>
+              {/* <CustomText color='white'>{selectedEvent.description}</CustomText> */}
+            </>
+          )}
+        </View>
+      </Modal>
+
+      <Modal isVisible={isImageModalVisible} 
+      backdropOpacity={1}
+      onBackdropPress={() => setImageModalVisible(false)}
+      >
+        <View style={{alignItems:'center'}}>
+          {selectedImage && (
+            <>
+              <Image source={{uri:selectedImage.image}} 
+                style={{width:Dimensions.get('window').width, height:Dimensions.get('window').height}} 
+                resizeMode='contain'/>
+              <TouchableOpacity style={{position:'absolute', top:30, right:10,width:30,height:30, backgroundColor:'white',borderRadius:50, alignItems:'center',justifyContent:'center'}} onPress={() => setImageModalVisible(false)}>
+                <CustomText fontWeight='bold' fontSize={24}>X</CustomText>
+              </TouchableOpacity>
+              <TouchableOpacity style={{position:'absolute',top:30,left:10}} onPress={()=>{Linking.openURL('https://www.google.de/maps/dir//'+selectedImage.lat+','+selectedImage.long)}}>
+                <FontAwesome5 name="directions" size={30} color="white" />
+              </TouchableOpacity>
+              <View style={{position:'absolute', bottom:20, marginBottom:20, padding:5, backgroundColor: 'grey',width:'50%',borderRadius:10, alignItems:'center'}}>
+                <CustomText fontWeight='bold'>{selectedImage.description}</CustomText>
+              </View>
+            </>
+          )}
+          
+          </View>
+      </Modal>
+
     </View>
+    
   );
 };
 
@@ -48,12 +122,3 @@ const styles = StyleSheet.create({
 });
 
 export default MapScreen;
-
-// //             {/* <MapView
-// //                 initialRegion={{
-// //                     latitude: 37.78825,
-// //                     longitude: -122.4324,
-// //                     latitudeDelta: 0.0922,
-// //                     longitudeDelta: 0.0421,
-// //                 }} 
-// //             />*/}
--- frontend/screens/pictures.tsx
+++ frontend/screens/pictures.tsx
@@ -1,11 +1,15 @@
-import {View, Text, StyleSheet, Image, FlatList, Dimensions} from 'react-native';
+import {View, Text, StyleSheet, Image, FlatList, Dimensions, TouchableOpacity} from 'react-native';
 import { useEffect, useState } from 'react';
 import client from '../actions/client';
 import { CustomText } from '../components/text';
 import { FontAwesome } from '@expo/vector-icons';
 import CustomButton from '../components/costum_botton';
+import Modal from "react-native-modal";
 
-const PictureScreen = () => {
+const PictureScreen = ({ route, navigation }) => {
+    
+    const [selectedPicture, setSelectedPicture] = useState(null);
+    const [isModalVisible, setModalVisible] = useState(false);
     const [pictureData,setPictureData] = useState([])
     const [loading, setLoading] = useState(true)
     useEffect(()=>{
@@ -15,19 +19,22 @@ const PictureScreen = () => {
 
 
     const renderItem = ({ item }) => (
-        <View style={styles.imageContainer}>
-            <Image source={item.image} style={styles.image}/>
+        <TouchableOpacity style={styles.imageContainer} 
+        onPress={() => {
+        setSelectedPicture(item);
+        setModalVisible(true);}}>
+            <Image source={{uri:item.image}} style={styles.image}/>
             <View style={styles.imageDescription}>
-                <View style={{width:'50%', alignItems:'flex-start',padding:10}}>
-                    <CustomText color='white'>{item.date}</CustomText>
-                    <CustomText color='white'>{item.description}  <FontAwesome name="map-marker" size={18} color="white" /> </CustomText>
+                <View style={{width:'65%', alignItems:'flex-start',padding:10}}>
+                    <CustomText color='white' fontSize={10}>{item.date}</CustomText>
+                    <CustomText color='white' fontSize={10}>{item.description}  <FontAwesome name="map-marker" size={18} color="white" /> </CustomText>
                 </View>
-                <View style={{width:'50%', alignItems:'flex-end',padding:10}}>
-                    <CustomText color='white'>by {item.username}</CustomText>
+                <View style={{width:'35%', alignItems:'flex-end',padding:10}}>
+                    <CustomText color='white' fontSize={10}>by {item.username}</CustomText>
                 </View>
                 
             </View>
-        </View>
+        </TouchableOpacity>
       );
 
 
@@ -35,7 +42,7 @@ const PictureScreen = () => {
         loading ? <Text>Loading..</Text>:(
             <View style={styles.container}>
                 <View style={{width:'20%'}}>
-                    <CustomButton text={'Take Picture'} onPress={()=>{}}/>
+                    <CustomButton text={'Take Picture'} onPress={()=>{navigation.navigate('Camera')}}/>
                 </View> 
                 <FlatList
                     data={pictureData}
@@ -44,6 +51,27 @@ const PictureScreen = () => {
                     numColumns={2}
                     showsVerticalScrollIndicator={false}
                 />
+                
+                <Modal isVisible={isModalVisible} 
+                backdropOpacity={1}
+                onBackdropPress={() => setModalVisible(false)}
+                >
+                    <View style={{alignItems:'center'}}>
+                    {selectedPicture && (
+                        <>
+                        <Image source={{uri:selectedPicture.image}} 
+                        style={{width:Dimensions.get('window').width, height:Dimensions.get('window').height}} 
+                        resizeMode='contain'/>
+                        <TouchableOpacity style={{position:'absolute', top:30, right:10,width:30,height:30, backgroundColor:'white',borderRadius:50, alignItems:'center',justifyContent:'center'}} onPress={() => setModalVisible(false)}>
+                            <CustomText fontWeight='bold' fontSize={24}>X</CustomText>
+                        </TouchableOpacity>
+                        <View style={{position:'absolute', bottom:20, marginBottom:20, padding:5, backgroundColor: 'grey',width:'50%',borderRadius:10, alignItems:'center'}}>
+                            <CustomText fontWeight='bold'>{selectedPicture.description}</CustomText>
+                        </View>
+                        </>
+                    )}
+                    </View>
+                </Modal>
             </View>
         )
     )
@@ -69,7 +97,7 @@ const styles = StyleSheet.create({
     },
     imageDescription: {
         flexDirection:'row',
-        top: -60,
+        top: -50,
         backgroundColor:'darkblue',
         borderBottomLeftRadius: 10,
         borderBottomRightRadius: 10,
