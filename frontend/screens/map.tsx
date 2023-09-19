f6550135736888f587bc8a0384d8902712f37ce2
Julian Hay <julian.hay@gmx.de>
2023-09-19 07:37:52 +0200

    adjust marker position, adde dloading animation

--- frontend/screens/map.tsx
+++ frontend/screens/map.tsx
@@ -7,6 +7,8 @@ import { CustomText } from '../components/text';
 import Modal from "react-native-modal";
 import { FontAwesome5 } from '@expo/vector-icons';
 import moment from 'moment';
+import FischLoading from '../components/loading';
+import { CloseButton } from '../components/custom_botton';
 
 const MapScreen = () => {
   const [selectedEvent, setSelectedEvent] = useState(null);
@@ -17,8 +19,8 @@ const MapScreen = () => {
   const [imageData,setImageData] = useState([])
   const [loading, setLoading] = useState(true)
   useEffect(()=>{
-      client.get('/events').then((res) => setEventData(res.data))
-      .then(() => client.get('/pictures').then((res) => setImageData(res.data)))
+      client.get('/events/').then((res) => setEventData(res.data))
+      .then(() => client.get('/pictures/').then((res) => setImageData(res.data)))
       .finally(() => setLoading(false))
   },[]) 
 
@@ -46,6 +48,8 @@ const MapScreen = () => {
       <Marker key={index} 
       coordinate={{longitude:parseFloat(image.long),latitude:parseFloat(image.lat)}}
       image={require('../assets/maps_marker.png')}
+      anchor={{x: 0.5, y: 1}}
+      centerOffset={{x: 0, y: -20}}
       onPress={()=>{
         setSelectedImage(image)
         setImageModalVisible(true)
@@ -54,7 +58,7 @@ const MapScreen = () => {
     ))
   
   return (
-    loading ? <CustomText>Loading...</CustomText> :
+    loading ? <FischLoading/> :
     <View style={styles.container}>
       <MapView style={styles.map}>
         {coloredCountries}
@@ -68,9 +72,10 @@ const MapScreen = () => {
         <View style={{alignItems:'center',height:'100%', justifyContent:'center'}}>
           {selectedEvent && (
             <>
-              <TouchableOpacity style={{position:'absolute', top:30, right:10,width:30,height:30, backgroundColor:'white',borderRadius:50, alignItems:'center',justifyContent:'center'}} onPress={() => setEventModalVisible(false)}>
-                <CustomText fontWeight='bold' fontSize={24}>X</CustomText>
-              </TouchableOpacity>
+              <View style={styles.closeButton}>
+                <CloseButton onPress={()=>{setEventModalVisible(false)}}/>
+              </View>
+
               <CustomText color='white' fontSize={24} fontWeight='bold'>{selectedEvent.title}</CustomText>
               <View style={{margin:-40}}>
                 <Image source={{uri:selectedEvent.image}} style={{width:Dimensions.get('window').width*0.4, height:Dimensions.get('window').height*0.4}} resizeMode='contain'/>
@@ -92,14 +97,17 @@ const MapScreen = () => {
               <Image source={{uri:selectedImage.image}} 
                 style={{width:Dimensions.get('window').width, height:Dimensions.get('window').height}} 
                 resizeMode='contain'/>
-              <TouchableOpacity style={{position:'absolute', top:30, right:10,width:30,height:30, backgroundColor:'white',borderRadius:50, alignItems:'center',justifyContent:'center'}} onPress={() => setImageModalVisible(false)}>
-                <CustomText fontWeight='bold' fontSize={24}>X</CustomText>
-              </TouchableOpacity>
-              <TouchableOpacity style={{position:'absolute',top:30,left:10}} onPress={()=>{Linking.openURL('https://www.google.de/maps/dir//'+selectedImage.lat+','+selectedImage.long)}}>
+              <View style={styles.closeButton}>
+                <CloseButton onPress={()=>{setImageModalVisible(false)}}/>
+              </View>
+              <TouchableOpacity style={{position:'absolute',top:'8%',left:'5%'}} onPress={()=>{Linking.openURL('https://www.google.de/maps/dir//'+selectedImage.lat+','+selectedImage.long)}}>
                 <FontAwesome5 name="directions" size={30} color="white" />
               </TouchableOpacity>
-              <View style={{position:'absolute', bottom:20, marginBottom:20, padding:5, backgroundColor: 'grey',width:'50%',borderRadius:10, alignItems:'center'}}>
+              <View style={{position:'absolute', bottom:60, padding:5, backgroundColor: 'grey',width:'50%',borderRadius:10, alignItems:'center'}}>
                 <CustomText fontWeight='bold'>{selectedImage.description}</CustomText>
+              </View>
+              <View style={{position:'absolute', bottom:20, marginBottom:20}}>
+                <CustomText fontSize={14} fontWeight='bold' color='white'>by {selectedImage.username}</CustomText>
               </View>
             </>
           )}
@@ -119,6 +127,11 @@ const styles = StyleSheet.create({
   map: {
     flex: 1,
   },
+  closeButton: {
+    position:'absolute', 
+    top:'8%', 
+    right:'5%',
+  }
 });
 
 export default MapScreen;
