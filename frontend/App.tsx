2f92ecffc4329405b959af2b1a28644467c29713
Julian Hay <julian.hay@gmx.de>
2023-09-19 07:43:47 +0200

    fix status bar color on android

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,9 +1,15 @@
 import 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
-import { StyleSheet } from 'react-native';
+import { StyleSheet, Platform } from 'react-native';
 import DrawerNavigator from './navigation/drawer';
 import { Provider } from 'react-redux';
 import store from './store';
+import {StatusBar} from 'expo-status-bar'
+import * as NavigationBar from 'expo-navigation-bar';
+
+if (Platform.OS == 'android') {
+  NavigationBar.setBackgroundColorAsync("darkblue");
+}
 
 export default function App() {
   return (
@@ -11,6 +17,7 @@ export default function App() {
       <NavigationContainer style={styles.app}>
           <DrawerNavigator/>
       </NavigationContainer>
+      <StatusBar style="light" backgroundColor='darkblue'/>
     </Provider>
     );
 }
