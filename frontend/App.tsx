4398770c0c5aa3ce1cc78b44d8e4078d4a2fba04
Julian Hay <julian.hay@gmx.de>
2023-10-15 10:11:47 +0200

    persist access token

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -2,10 +2,12 @@ import 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
 import { StyleSheet, Platform } from 'react-native';
 import DrawerNavigator from './navigation/drawer';
+import {store, persistor} from './store';
 import { Provider } from 'react-redux';
-import store from './store';
+import { PersistGate } from 'redux-persist/integration/react'
 import {StatusBar} from 'expo-status-bar'
 import * as NavigationBar from 'expo-navigation-bar';
+import FischLoading from './components/loading';
 
 if (Platform.OS == 'android') {
   NavigationBar.setBackgroundColorAsync("darkblue");
@@ -14,10 +16,12 @@ if (Platform.OS == 'android') {
 export default function App() {
   return (
     <Provider store={store}>
-      <NavigationContainer style={styles.app}>
-          <DrawerNavigator/>
-      </NavigationContainer>
-      <StatusBar style="light" backgroundColor='darkblue'/>
+      <PersistGate loading={<FischLoading/>} persistor={persistor}>
+        <NavigationContainer style={styles.app}>
+            <DrawerNavigator/>
+        </NavigationContainer>
+        <StatusBar style="light" backgroundColor='darkblue'/>
+      </PersistGate>
     </Provider>
     );
 }
--- frontend/store.tsx
+++ frontend/store.tsx
@@ -2,15 +2,24 @@ import { createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { persistStore, persistReducer } from 'redux-persist'
 
 const initialState = {};
 
 const middleware = [thunk];
 
-const store = createStore(
-    rootReducer,
+const persistConfig = {
+    key: 'root',
+    storage: AsyncStorage,
+  }
+  
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+  
+export const store = createStore(
+    persistedReducer,
     initialState,
     composeWithDevTools(applyMiddleware(...middleware))
 );
 
-export default store;
+export const persistor = persistStore(store)
