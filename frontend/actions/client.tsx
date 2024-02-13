b4755ea52d6104d32a0501c2169a0b5509378155
Julian Hay <julian.hay@gmx.de>
2024-02-13 10:27:07 +0100

    adjust indent format

--- frontend/actions/client.tsx
+++ frontend/actions/client.tsx
@@ -1,20 +1,20 @@
 import axios from "axios";
-import {store} from "../store";
+import { store } from "../store";
 // const client = axios.create({baseURL: "http://127.0.0.1:8000/app"})
 
-const client = axios.create({baseURL: "https://wodkafis.ch/app"})
+const client = axios.create({ baseURL: "https://wodkafis.ch/app" });
 
 client.interceptors.request.use(
-    function(config) {
-      const token = store.getState().auth.token;
-      if (token) {
-        config.headers["Authorization"] = 'Token ' + token;
-      }
-      return config;
-    },
-    function(error) {
-      return Promise.reject(error);
+  function (config) {
+    const token = store.getState().auth.token;
+    if (token) {
+      config.headers["Authorization"] = "Token " + token;
     }
-  );
+    return config;
+  },
+  function (error) {
+    return Promise.reject(error);
+  }
+);
 
-export default client
+export default client;
