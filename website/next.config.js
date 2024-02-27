74adce7c76c27cfd6f33ef09c2c962e45a849681
Julian Hay <julian.hay@gmx.de>
2024-02-27 14:07:23 +0100

    added image urls

--- website/next.config.js
+++ website/next.config.js
@@ -1,4 +1,22 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+
+    images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'www.wodkafis.ch',
+        port: '',
+        pathname: '/media/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'play.google.com',
+        port: '',
+        pathname: '/intl/en_us/badges/static/images/badges/**',
+      },
+    ],
+  },
+}
 
 module.exports = nextConfig
